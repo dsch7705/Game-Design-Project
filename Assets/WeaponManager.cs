ac5db7371aa7436ff415ea2f9c46c8739a5f3586
Apriquat <dschooner23@eastwoodschools.org>
2021-09-10 10:53:09 -0400

    nothing

--- Assets/WeaponManager.cs
+++ Assets/WeaponManager.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class WeaponManager : MonoBehaviour
 {
 
-    public WeaponClass currentWeaponClass;
+    public (WeaponClass, int) currentWeaponClass;
+    private int _currentWeaponClass;
     
     public List<WeaponClass> weapons = new List<WeaponClass>();
     void Start()
     {
         weapons.Add(new WeaponClass("Pistol", WeaponClass.FireMode.semi, 10f));
-        currentWeaponClass = weapons[0];
+        currentWeaponClass.Item1 = weapons[0];
+        currentWeaponClass.Item2 = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (weapons.Count + 1 > weapons.IndexOf(currentWeaponClass))
+        if (Input.GetAxis("Switch Weapon") != 0 && weapons.Count + 1 > weapons.IndexOf(currentWeaponClass.Item1))
+        {
+            currentWeaponClass.Item1 = weapons[weapons.IndexOf(currentWeaponClass.Item1) + 1];
+            currentWeaponClass.Item2, _currentWeaponClass = weapons.IndexOf(currentWeaponClass.Item1);
+        }
     }
 }
